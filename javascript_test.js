b5dffaaf187fccb1addfc09ac7fa9e344406784b
Spookster12 <joelteeple3@gmail.com>
2025-08-27 18:00:00 -0400

    finished function assignment

--- javascript_test.js
+++ javascript_test.js
@@ -1,10 +1,15 @@
-alert("hello world") //makes alert
+// alert("hello world") //makes alert
 
 
 
-var a="beginning string" + "ending string" //two string variables
+// var a="beginning string" + "ending string" //two string variables
 
-document.write(3+3); //is an expression
+// document.write(3+3); //is an expression
 
-document.write(a);
+// document.write(a);
 
+function test_fun(){
+    var a="beginning string" + "ending string";
+    document.write(a);
+    document.getElementById('test').innerHTML=a;
+}
